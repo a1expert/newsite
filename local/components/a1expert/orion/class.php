87809b5bae1c48ab67e701a0bda442f9a774544a
zkkorion <zkkorion@gmail.com>
2018-11-06 10:22:49 +0500

    a

--- local/components/a1expert/orion/class.php
+++ local/components/a1expert/orion/class.php
@@ -1,28 +1,52 @@
 <?
 use Bitrix\Iblock\Component\Base;
-
 class Orion extends Base
 {
     #region fields
-    public $elFilter;
-    public $elSelect;
-    public $elOrder;
-    public $editorButton;
+    private $elFilter;
+    private $elSelect;
+    private $elOrder;
+    private $editorButton;
     #endregion fields
+    #region properties
+    public function elFilter($value)
+    {
+        if(!empty($value))
+            $this->elFilter = $value;
+        else
+            return $this->elFilter;    
+    }
+    public function elSelect($value)
+    {
+        if(!empty($value))
+            $this->elSelect = $value;
+        else
+            return $this->elSelect;    
+    }
+    public function elOrder($value)
+    {
+        if(!empty($value))
+            $this->elOrder = $value;
+        else
+            return $this->elOrder;    
+    }
+    #endregion properties
 
-    public function __construct($component, $active = "Y")
-	{
+    public function __construct($component)
+    {
         parent::__construct($component);
-        $this->elFilter["ACTIVE"] = $active;
-        $this->elSelect = array("IBLOCK_ID", "ID");
-        $this->elOrder = array("sort"=>"asc");
-	}
+        $this->elSelect(array());
+    }
 
     function getIblockElements($elementIterator){}
     function chooseOffer($offers, $iblockId){}
     function getAdditionalCacheId(){}
     function getComponentCachePath(){}
 
+    /**
+     * Статичная функция очистки массива от пустых значений. Работает рекурсивно, очищает полностью весь массив включая вложенные. Ничего не возвращает
+     * @var array $array Массив для очистки, передается по ссылке.
+     */
     public function CleanArray(&$array)
     {
         foreach ($array as $k=>$v)
@@ -37,10 +61,9 @@ class Orion extends Base
     {
         if(empty($params["IBLOCK_ID"]))
             throw new Exception("Не заполнен обязательный параметр - Инфоблок");
-        $this->elFilter["IBLOCK_ID"] = $params["IBLOCK_ID"];
+        $this->elFilter(array("IBLOCK_ID"=>$params["IBLOCK_ID"]));
         if(!empty($params["ELEMENT_FIELDS_FILTER"]))
         {
-            $this->CleanArray($params["ELEMENT_FIELDS_FILTER"]);
             $this->elFilter = array_merge($this->elFilter, $params["ELEMENT_FIELDS_FILTER"]);
         }
     }
@@ -81,8 +104,11 @@ class Orion extends Base
         $dbres = CIBlockElement::GetList($this->elOrder, $this->elFilter, $arGroupBy, $arNavStartParams, $this->elSelect);
         while ($arItem = $dbres->GetNextElement())
         {  
-            $item = $arItem->GetFields();
-            $item["PROPERTIES"] = $arItem->GetProperties();
+            $item = $arItem->fields;
+            if(!empty($item["PREVIEW_PICTURE"]))
+                $item["PREVIEW_PICTURE"] = CFile::GetFileArray($item["PREVIEW_PICTURE"]);
+            if(!empty($item["DETAIL_PICTURE"]))
+                $item["DETAIL_PICTURE"] = CFile::GetFileArray($item["DETAIL_PICTURE"]);
             if($this->editorButton)
             {
                 $arButtons = CIBlock::GetPanelButtons($item["IBLOCK_ID"], $item["ID"], 0, array("SECTION_BUTTONS"=>false, "SESSID"=>false));
@@ -93,7 +119,23 @@ class Orion extends Base
         }
         return array("ITEMS" => $result);
     }
-    
+    public function ResToJson($arResult)
+    {
+        $arrJson = array();
+        $jsonItem = array("image"=>"", "name"=>"", "seats"=>"", "type"=>"", "price"=>"", "label"=>"");
+        foreach ($arResult["ITEMS"] as $arItem)
+        {
+            $jsonItem["image"]=$arItem["DETAIL_PICTURE"];
+            $jsonItem["name"]=$arItem["NAME"];
+            $jsonItem["seats"]=$arItem["PROPERTY_WIDTH_VALUE"];
+            $jsonItem["type"]=$arItem["PROPERTY_DENSITY_VALUE"];
+            $jsonItem["price"]=$arItem["PROPERTY_LENGTH_VALUE"];
+            $jsonItem["label"]=$arItem["PROPERTY_IMPREG_VALUE"];
+            $arrJson[]=$jsonItem;
+        }
+        $this->jsonString = json_encode($arrJson, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        
+    }
     public function executeComponent()
     {
         $this->editorButton = ($this->arParams["DISPLAY_EDITOR_BUTTONS"] === "Y") ? true : false;
